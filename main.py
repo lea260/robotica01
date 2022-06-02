a83d3c4b199648392cabe347f62cda74499638f2
leandro <lcalvetti1@gmail.com>
2022-06-02 19:47:26 -0300

    seguidor 2.0

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port
-from pybricks.tools import wait
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# Initialize the motors.
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+# Iniciar motor en puerto A y D
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 
-# Initialize the color sensor.
+# Inicializar el sensor de luz
 line_sensor = ColorSensor(Port.S1)
 
-# Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+# Inicializar DriveBase()
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=170)
 
-# Calculate the light threshold. Choose values based on your measurements.
-#BLACK = 9
-#WHITE = 85
-#threshold = (BLACK + WHITE) / 2
+# Declarar velocidad de motores
+DRIVE_SPEED = 100
 
-# Set the drive speed at 100 millimeters per second.
-#DRIVE_SPEED = 100
+# Calcular el limite de luz
+BLACK = 11
+WHITE = 97
+limite = (BLACK + WHITE) / 2
 
-# Set the gain of the proportional line controller. This means that for every
-# percentage point of light deviating from the threshold, we set the turn
-# rate of the drivebase to 1.2 degrees per second.
-
-# For example, if the light value deviates from the threshold by 10, the robot
-# steers at 10*1.2 = 12 degrees per second.
+# Declarar ganancias proporcional
 #PROPORTIONAL_GAIN = 1.2
 
-# Start following the line endlessly.
-while True:
+# Write your program here.
+ev3.speaker.beep(3000, 0.5)
+
+while True :
+    # Calcula la desviacion
     sensor=line_sensor.reflection()
-    screen.draw_text(x, y, sensor, text_color=Color.BLACK, background_color=None)
-    # Calculate the deviation from the threshold.
-    #deviation = line_sensor.reflection() - threshold
+    ev3.screen.draw_text(0, 0, sensor, Color.BLACK, None)
+    desviacion = line_sensor.reflection() - limite
 
-    # Calculate the turn rate.
-    #turn_rate = PROPORTIONAL_GAIN * deviation
+    # Valor de giro
+    valor_giro = desviacion
 
-    # Set the drive base speed and turn rate.
-    #robot.drive(DRIVE_SPEED, turn_rate)
-
-    # You can wait for a short time or do other things in this loop.
+    # Robot avanza
+    robot.drive(DRIVE_SPEED, valor_giro)
