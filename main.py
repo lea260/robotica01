7b576b3778f81a1efaab876277c4c4000222d004
Leandro <lcalvetti1@gmail.com>
2022-06-16 19:31:22 -0300

    version 2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-from ast import Pass
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -7,10 +6,6 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
 
 
 # Create your objects here.
@@ -51,12 +46,3 @@ while True:
         izquierdo.stop() 
         derecho.run(VELOCIDAD)
         
-        
-
-    # desviacion = line_sensor.reflection() - umbral
-
-    # Valor de giro
-    # valor_giro = desviacion
-
-    # Robot avanza
-    #robot.drive(DRIVE_SPEED, valor_giro)
